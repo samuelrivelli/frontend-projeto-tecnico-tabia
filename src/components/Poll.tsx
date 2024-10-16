36772cf3409c161bbdaf500ee9901b5865b91ae3
Samuel Rivelli <samuelmagrivelli@gmail.com>
2024-10-16 17:43:14 -0300

    mudanca no home css

--- src/components/Poll.tsx
+++ src/components/Poll.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
-import axios from "../axios";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../css/Home.css";
+import "../css/Poll.css";
 
 interface Option {
   id: number;
@@ -17,40 +16,8 @@ interface PollProps {
 }
 
 const Poll: React.FC<PollProps> = ({ id, title, description, options }) => {
-  const [selectedOptionId, setSelectedOptionId] = useState<number | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [success, setSuccess] = useState<string | null>(null);
-  const [pollOptions, setPollOptions] = useState<Option[]>(options);
+ 
   const navigate = useNavigate();
-
-  const handleVote = async () => {
-    if (selectedOptionId === null) {
-      setError("Por favor, selecione uma opção para votar.");
-      return;
-    }
-
-    try {
-      const voteData = {
-        userId: localStorage.getItem("userId"),
-        optionId: selectedOptionId,
-        pollId: id,
-      };
-
-      await axios.post("/api/v1/votes", voteData);
-      setPollOptions((prevOptions) =>
-        prevOptions.map((option) =>
-          option.id === selectedOptionId
-            ? { ...option, voteCount: option.voteCount + 1 }
-            : option
-        )
-      );
-      setSuccess("Voto enviado com sucesso!");
-      setSelectedOptionId(null);
-    } catch (err) {
-      console.error("Erro ao enviar voto:", err);
-      setError("Ocorreu um erro ao enviar seu voto. Tente novamente.");
-    }
-  };
 
   const navigateToComments = () => {
     navigate(`/poll/${id}/comments`); 
